989500ba1ead04a6c51f43d0b266b5699bbe8602
warren clark <wclarkcito@gmail.com>
2021-05-06 13:58:15 -0700

    search is working

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,7 @@ import API from '../utils/API';
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EmployeeDetail from './EmployeeDetail';
-import { Navbar } from 'react-bootstrap'
+
 
 
 export default class Employee extends Component {
@@ -37,33 +37,31 @@ export default class Employee extends Component {
 
     searchEmployees = search => {
         const query = search.toLowerCase();
-        if (!query) {
-            this.setState({ queryResults: this.state.results })
-        } else {
+        if (query) {
             let apiResults = this.state.results;
-            apiResults = apiResults.filter(item => item.name.toLowerCase().includes(query))
+            apiResults = apiResults.filter(item => item.name.first.toLowerCase().includes(query))
+            this.setState({ queryResults: apiResults })
+        } else {
+            this.setState({ queryResults: this.state.results })
         }
     }
 
-
+    handleSearch = (event) => {
+        console.log(event.target.value)
+        this.setState({ search: event.target.value })
+        this.searchEmployees(this.state.search)
+    }
 
     render() {
         return (
             <div>
-                <Navbar>
-                    <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>
-                            Signed in as: <a href="#login">Mark Otto</a>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Navbar>
+
 
 
                 <div className="jumbotron">
                     <h1>Employee Directory</h1>
                 </div>
+                <input type="text" onChange={this.handleSearch} value={this.state.search} />
                 <table>
                     <tr>
                         <th>img</th>
@@ -71,16 +69,19 @@ export default class Employee extends Component {
                         <th>last</th>
                         <th>phone</th>
                         <th>email</th>
+                        <th>city</th>
 
                         <th>state</th>
                     </tr>
-                    {this.state.results.map((employee) => {
+                    {this.state.queryResults.map((employee) => {
+
                         return <EmployeeDetail first={employee.name.first}
                             last={employee.name.last}
                             thumbnail={employee.picture.thumbnail}
                             email={employee.email}
                             phone={employee.cell}
-                            location={employee.location.state}
+                            city={employee.location.city}
+                            state={employee.location.state}
                         />
                     })}
 
--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 
 export default class EmployeeDetail extends Component {
     render(props) {
+
         return (
             <tr>
                 {/* .map on state results output a row in table(component) */}
@@ -12,8 +13,9 @@ export default class EmployeeDetail extends Component {
                 <td>{this.props.last}</td>
                 <td>{this.props.phone}</td>
                 <td>{this.props.email}</td>
+                <td>{this.props.city}</td>
 
-                <td>{this.props.location}</td>
+                <td>{this.props.state}</td>
             </tr>
 
 
